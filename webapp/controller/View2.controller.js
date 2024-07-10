f92ac59cb20dbf1fd01dc489aa09af3530d8adf2
ArwaElziainy <q.m11@icloud.com>
2024-07-10 14:45:32 +0200

    10/7

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/History",
-		"sap/m/MessageToast"
+	"sap/m/MessageToast"
 ], function(Controller, History, MessageToast) {
 	"use strict";
 
@@ -11,7 +11,6 @@ sap.ui.define([
 			oRouter.getRoute("View2").attachMatched(this._onRouteMatched, this);
 		},
 		_onRouteMatched: function(oEvent) {
-
 			var sServiceUri = "https://dev.monairy.com/sap/opu/odata/SAP/ZGW_SC_SRV/";
 
 			var oModel = new sap.ui.model.odata.ODataModel(sServiceUri, {
@@ -28,59 +27,100 @@ sap.ui.define([
 					this.getView().byId("genStatus").setText(oData2.ZStatusSta);
 					// General Information
 
+					this.getView().byId("shiftTex").setText(oData2.Zshift);
 					this.getView().byId("shift").setValue(oData2.Zshift);
+					this.getView().byId("dateTex").setText(oData2.ZdateGluFru);
 					this.getView().byId("date").setValue(oData2.ZdateGluFru);
 					//تحويلات الجلوكوز
+					this.getView().byId("awl1Tex").setText(oData2.ZTank1Sta);
 					this.getView().byId("awl1").setValue(oData2.ZTank1Sta);
+					this.getView().byId("akher1Tex").setText(oData2.ZTank3Sta);
 					this.getView().byId("akher1").setValue(oData2.ZTank3Sta);
+					this.getView().byId("y1Tex").setText(oData2.ZTank5Sta);
 					this.getView().byId("y1").setValue(oData2.ZTank5Sta);
+					this.getView().byId("awl2Tex").setText(oData2.ZTank2Sta);
 					this.getView().byId("awl2").setValue(oData2.ZTank2Sta);
+					this.getView().byId("akher2Tex").setText(oData2.ZTank4Sta);
 					this.getView().byId("akher2").setValue(oData2.ZTank4Sta);
+					this.getView().byId("y2Tex").setText(oData2.ZTank6Sta);
 					this.getView().byId("y2").setValue(oData2.ZTank6Sta);
+					this.getView().byId("brTex").setText(oData2.ZTank7Sta);
 					this.getView().byId("br").setValue(oData2.ZTank7Sta);
+					this.getView().byId("totalTex").setText(oData2.ZFru1Sta);
 					this.getView().byId("total").setValue(oData2.ZFru1Sta);
 					//النشا الجاف
-					// this.getView().byId("orderNo").setValue(oData2.);
+					this.getView().byId("HTex").setText(oData2.ZDry1Sta);
 					this.getView().byId("H").setValue(oData2.ZDry1Sta);
+					this.getView().byId("ITex").setText(oData2.ZDry2Sta);
 					this.getView().byId("I").setValue(oData2.ZDry2Sta);
 					//النشا التام
-					// this.getView().byId("orderNo1").setValue(oData2.);
+					this.getView().byId("KTex").setText(oData2.ZPack1Sta);
 					this.getView().byId("K").setValue(oData2.ZPack1Sta);
+					this.getView().byId("LTex").setText(oData2.ZPack2Sta);
 					this.getView().byId("L").setValue(oData2.ZPack2Sta);
+					this.getView().byId("MTex").setText(oData2.ZPack3Sta);
 					this.getView().byId("M").setValue(oData2.ZPack3Sta);
 					// فرق تغير تانكات حليب النشا
-					// this.getView().byId("orderNo2").setValue(oData2.);
+					this.getView().byId("T1Tex").setText(oData2.ZTank18Sta);
 					this.getView().byId("T1").setValue(oData2.ZTank18Sta);
-					// this.getView().byId("KM").setValue(oData2.ZBy1Sta);
+					this.getView().byId("KMTex").setText(oData2.ZBy1Sta);
+					this.getView().byId("KM").setValue(oData2.ZBy1Sta);
+					this.getView().byId("T2Tex").setText(oData2.ZTank14Sta);
 					this.getView().byId("T2").setValue(oData2.ZTank14Sta);
-					// this.getView().byId("GM").setValue(oData2.ZBy4Sta);
+					this.getView().byId("GMTex").setText(oData2.ZBy4Sta);
+					this.getView().byId("GM").setValue(oData2.ZBy4Sta);
+					this.getView().byId("T3Tex").setText(oData2.ZTank15Sta);
 					this.getView().byId("T3").setValue(oData2.ZTank15Sta);
-					// this.getView().byId("JM").setValue(oData2.ZBy7Sta);
+					this.getView().byId("JMTex").setText(oData2.ZBy7Sta);
+					this.getView().byId("JM").setValue(oData2.ZBy7Sta);
+					this.getView().byId("T4Tex").setText(oData2.ZTank16Sta);
 					this.getView().byId("T4").setValue(oData2.ZTank16Sta);
-					// this.getView().byId("Oil").setValue(oData2.ZBy10Sta);
+					this.getView().byId("OilTex").setText(oData2.ZBy10Sta);
+					this.getView().byId("Oil").setValue(oData2.ZBy10Sta);
+					this.getView().byId("T5Tex").setText(oData2.ZTank17Sta);
 					this.getView().byId("T5").setValue(oData2.ZTank17Sta);
-					// this.getView().byId("W").setValue(oData2.ZBy14Sta);
+					this.getView().byId("WTex").setText(oData2.ZBy14Sta);
+					this.getView().byId("W").setValue(oData2.ZBy14Sta);
 					//Table
+					this.getView().byId("production1Tex").setText(oData2.ZQty1Sta);
 					this.getView().byId("production1").setValue(oData2.ZQty1Sta);
+					this.getView().byId("oil1Tex").setText(oData2.ZOil1Sta);
 					this.getView().byId("oil1").setValue(oData2.ZOil1Sta);
+					this.getView().byId("production2Tex").setText(oData2.ZQty2Sta);
 					this.getView().byId("production2").setValue(oData2.ZQty2Sta);
+					this.getView().byId("oil2Tex").setText(oData2.ZOil2Sta);
 					this.getView().byId("oil2").setValue(oData2.ZOil2Sta);
+					this.getView().byId("production3Tex").setText(oData2.ZQty3Sta);
 					this.getView().byId("production3").setValue(oData2.ZQty3Sta);
+					this.getView().byId("oil3Tex").setText(oData2.ZOil3Sta);
 					this.getView().byId("oil3").setValue(oData2.ZOil3Sta);
+					this.getView().byId("production4Tex").setText(oData2.ZQty4Sta);
 					this.getView().byId("production4").setValue(oData2.ZQty4Sta);
+					this.getView().byId("oil4Tex").setText(oData2.ZOil4Sta);
 					this.getView().byId("oil4").setValue(oData2.ZOil4Sta);
 					//الباقي
+					this.getView().byId("KKTex").setText(oData2.ZKkSta);
 					this.getView().byId("KK").setValue(oData2.ZKkSta);
+					this.getView().byId("SSTex").setText(oData2.ZSsSta);
 					this.getView().byId("SS").setValue(oData2.ZSsSta);
+					this.getView().byId("ProtinTex").setText(oData2.ZQtyproSta);
 					this.getView().byId("Protin").setValue(oData2.ZQtyproSta);
+					this.getView().byId("HumRTex").setText(oData2.ZHumSta);
 					this.getView().byId("HumR").setValue(oData2.ZHumSta);
 					// ---------------
+					this.getView().byId("firstTex").setText(oData2.ZWat1Sta);
 					this.getView().byId("first").setValue(oData2.ZWat1Sta);
+					this.getView().byId("lastTex").setText(oData2.ZWat2Sta);
 					this.getView().byId("last").setValue(oData2.ZWat2Sta);
+					this.getView().byId("WTex").setText(oData2.ZWat3Sta);
 					this.getView().byId("WY").setValue(oData2.ZWat3Sta);
+					this.getView().byId("WHumTex").setText(oData2.ZWatHumSta);
 					this.getView().byId("WHum").setValue(oData2.ZWatHumSta);
+					this.getView().byId("PKTex").setText(oData2.ZBy1Sta);
 					this.getView().byId("PK").setValue(oData2.ZBy1Sta);
+					this.getView().byId("PGTex").setText(oData2.ZBy4Sta);
 					this.getView().byId("PG").setValue(oData2.ZBy4Sta);
+					this.getView().byId("GTTex").setText(oData2.ZBy7Sta);
 					this.getView().byId("GT").setValue(oData2.ZBy7Sta);
 					//Outputs
 					this.getView().byId("RefNo").setText(oData2.ZdateSta);
@@ -117,13 +157,25 @@ sap.ui.define([
 					this.getView().byId("WW2").setText(oData2.ZBy23Sta);
 					this.getView().byId("by").setText(oData2.ZdateStaBy);
 					this.getView().byId("on").setText(oData2.ZdateStaOn);
-
+					
 				}.bind(this),
 				error: function(oError) {
 					console.log("error")
 				}
 			});
 
+		},
+		onChange: function() {
+			var editIcon = document.getElementsByClassName("onEdit");
+			for (var i = 0; i < editIcon.length; i++) {
+				editIcon[i].style.display = "none";
+			}
+			this.getView().byId("save2").setVisible(true);
+			var displayBlock = document.getElementsByClassName("displayNone");
+			for (var i2 = 0; i2 < displayBlock.length; i2++) {
+				displayBlock[i2].style.display = "Block";
+			}
+			console.log(displayBlock)
 		},
 
 		onEdit: function() {
@@ -132,7 +184,6 @@ sap.ui.define([
 
 			var refNo = this.getView().byId("genRef");
 			var refValue = refNo.getText();
-		
 
 			var oUpdatedData = {
 				// General Details
@@ -148,16 +199,16 @@ sap.ui.define([
 				ZTank7Sta: +(this.getView().byId("br").getValue()),
 				ZFru1Sta: +(this.getView().byId("total").getValue()),
 				//النشا الجاف
-				// : +(this.getView().byId("orderNo").getValue()),
+			
 				ZDry1Sta: +(this.getView().byId("H").getValue()),
 				ZDry2Sta: +(this.getView().byId("I").getValue()),
 				//النشا التام
-				// : +(this.getView().byId("orderNo1").getValue()),
+			
 				ZPack1Sta: +(this.getView().byId("K").getValue()),
 				ZPack2Sta: +(this.getView().byId("L").getValue()),
 				ZPack3Sta: +(this.getView().byId("M").getValue()),
 				// فرق تغير تانكات حليب النشا
-				// : +(this.getView().byId("orderNo2").getValue()),
+		
 				ZTank18Sta: +(this.getView().byId("T1").getValue()),
 				// ZBy1Sta: +(this.getView().byId("KM").getValue()),
 				ZTank14Sta: +(this.getView().byId("T2").getValue()),
@@ -178,6 +229,7 @@ sap.ui.define([
 				ZQty4Sta: +(this.getView().byId("production4").getValue()),
 				ZOil4Sta: +(this.getView().byId("oil4").getValue()),
 				//الباقي
+
 				ZKkSta: +(this.getView().byId("KK").getValue()),
 				ZSsSta: +(this.getView().byId("SS").getValue()),
 				ZQtyproSta: +(this.getView().byId("Protin").getValue()),
@@ -195,17 +247,27 @@ sap.ui.define([
 
 			oModel.update("/SCHeaderSet(" + refValue + ")", oUpdatedData, {
 				success: function() {
-						MessageToast.show("Refrence Number: " + refValue + " Updated successfully");
+					MessageToast.show("Refrence Number: " + refValue + " Updated successfully");
+					var displayBlock = document.getElementsByClassName("displayNone");
+					for (var i2 = 0; i2 < displayBlock.length; i2++) {
+						displayBlock[i2].style.display = "none";
+					}
+					var editIcon = document.getElementsByClassName("onEdit");
+					for (var i = 0; i < editIcon.length; i++) {
+						editIcon[i].style.display = "Block";
+					}
+					this.getView().byId("save2").setVisible(false);
+
 				},
 				error: function(oError) {
 					console.log(oError);
 				}
 			});
 		},
-			onCreate: function(oEvent) {
+		onCreate: function(oEvent) {
 
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-		
+
 			oRouter.navTo("View3");
 		},
 		onNavBack: function() {
